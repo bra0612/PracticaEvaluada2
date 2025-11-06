746aa5e75a9bfe7af75a71965cf3827ed6950488
Brayan Molina Pizarro <molinapizarrobrayan@gmail.com>
2025-11-05 20:46:59 -0600

    Add files via upload

---
+++ CasoPractico2/src/casopractico2/productos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package casopractico2;
+import javax.swing.JOptionPane;
+
+/**
+ 
+ * @author Any Pizarro
+ */
+public class productos {
+    int productos [][]= new int[][];
+    
+    
+   
+    
+}
+
+
